fbf243f2098f3d6a17ce263e81fa161b81cc37cc
Santhi-ya <santhiyasr2005@gamil.com>
2026-04-23 21:20:11 +0530

    just upadted

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express =require("express");
 const mongoose =require("mongoose");
+const cors = require ("cors");
 require("dotenv").config();
 const app=express();
-
+app.use(cors());
 app.use(express.json());
 
 
@@ -55,6 +56,6 @@ app.delete("/todos/:id", async(req,res)=>{await Todo.findByIdAndDelete(req.param
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT || 3000,()=>{
+app.listen(PORT || 5000,()=>{
     console.log(`Server Running on port {$ PORT}`);
 });
